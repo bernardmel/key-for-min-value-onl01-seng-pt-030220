58598713337c265a3ce6a9459f6ed25be884109d
Bernard  Mel <bernard.mamuyac@gmail.com>
2020-03-13 01:51:37 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,18 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+cur_min_val = nil
+key_min_val = nil
+  name_hash.each do |key, value|
+    if cur_min_val == nil
+       cur_min_val = value
+       key_min_val = key
+     else
+       if cur_min_val > value
+          cur_min_val = value
+          key_min_val = key
+        end
+      end
+end
+key_min_val
 end
